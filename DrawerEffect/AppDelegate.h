25c7d2d6134cc35911af2fc56beb83e93bd742e0
totem <--edit>
2015-11-12 10:27:30 +0800

    Initial Commit

---
+++ DrawerEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawerEffect
+//
+//  Created by 郭晨风 on 15/11/12.
+//  Copyright © 2015年 郭晨风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawerEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawerEffect
+//
+//  Created by 郭晨风 on 15/11/12.
+//  Copyright © 2015年 郭晨风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
